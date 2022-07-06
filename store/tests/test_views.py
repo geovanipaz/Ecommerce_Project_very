a10ab1e3af395d3c0f3391612527ebc91a941384
Geovani Paz <geovanipaz7@gmail.com>
2022-07-06 15:45:58 -0300

    teste de views

---
+++ store/tests/test_views.py
@@ -0,0 +1,60 @@
+
+from unittest import skip
+from urllib import request, response
+from django.urls import reverse
+from django.http import HttpRequest
+
+from django.contrib.auth.models import User
+
+from store.models import Category, Product
+from django.test import Client, RequestFactory, TestCase
+from store.views import all_products
+'''
+@skip("demonstrating skipping")
+class TestSkip(TestCase):
+    def test_skip_exmaple(self):
+        pass
+'''
+class TestViewResponse(TestCase):
+    def setUp(self) -> None:
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                               slug='django-beginners', price='20.00', image='django')
+        
+    def test_url_allowed_hosts(self):
+        """teste allowed host
+        """
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_product_detail_url(self):
+        response = self.c.get(reverse('store:product_detail', args=['django-beginners']))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_category_detail_url(self):
+        """
+        Test category response status
+        """
+        response = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        print(html)
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_view_function(self):
+        request = self.factory.get('/item/django-beginners')
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        print(html)
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
-    path('item/<slug>/', views.product_detail, name='product_detail')
+    path('item/<slug>/', views.product_detail, name='product_detail'),
+    path('search/<category_slug>/', views.category_list, name='category_list')
 ]
